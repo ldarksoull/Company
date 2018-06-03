f3159818c03fe9b9f02389bc83a5b2b59f16c07a
Михаил <ldarksoull@yandex.ru>
2018-06-03 13:38:44 +0300

    11

--- js/script.js
+++ js/script.js
@@ -20,17 +20,52 @@ $(document).ready(function () {
 });
 //accordeon
 
-
-$( function() {
-    var icons = {
-      header: "icon-arrow-down",
-      activeHeader: "icon-arrow-up"
-    };
-    $("#accordion" ).accordion({
-      icons: icons,
-      heightStyle: "content" 
-    });
-    $( "#accordion" ).accordion( "option", "active", 1 )
-    $( "#accordion" ).accordion( "option", "header", "div" );
-    $( "#accordion" ).accordion( "option", "heightStyle", "content" );
-  } );
+  $('.ui.accordion').accordion();
+  $('.accordion')
+  .accordion({
+    selector: {
+      trigger: '.title'
+    }
+  })
+;
+$('.title1').click(function(){
+    if(!$(this).hasClass('active')){
+        $('.title1 .icon').addClass('icon-change');
+    }
+    else{
+        $('.title1 .icon').removeClass('icon-change');
+    }
+})
+$('.title2').click(function(){
+    if(!$(this).hasClass('active')){
+        $('.title2 .icon').addClass('icon-change');
+    }
+    else{
+        $('.title2 .icon').removeClass('icon-change');
+    }
+})
+$('.title3').click(function(){
+    if(!$(this).hasClass('active')){
+        $('.title3 .icon').addClass('icon-change');
+    }
+    else{
+        $('.title3.icon').removeClass('icon-change');
+    }
+})
+$('.title4').click(function(){
+    if(!$(this).hasClass('active')){
+        $('.title4 .icon').addClass('icon-change');
+    }
+    else{
+        $('.title4 .icon').removeClass('icon-change');
+    }
+})
+$('.title5').click(function(){
+    if(!$(this).hasClass('active')){
+        $('.title5 .icon').addClass('icon-change');
+    }
+    else{
+        $('.title5 .icon').removeClass('icon-change');
+    }
+})
+ 
