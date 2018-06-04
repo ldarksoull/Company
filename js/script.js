bd42c7ec3e0919812146d84b3974578a6c4c37d4
Михаил <ldarksoull@yandex.ru>
2018-06-04 20:52:46 +0300

    аккордион футер

--- js/script.js
+++ js/script.js
@@ -1,93 +1,76 @@
 //Плавное выпадающее меню
 $(document).ready(function () {
-    // var compare = $('.compare-wrapper');
-    // var lk = $('.lk-wrapper');
-    // var phone = $('.header__phone');
     $("#menu-btn").click(function () {
         $("#menu").slideToggle('fast');
         $(this).toggleClass("active");
-        if ($('#menu-btn').hasClass('active')){
+        if ($('#menu-btn').hasClass('active')) {
             $('.fa-times').show()
             $('.fa-bars').hide()
-        }
-        else{
+        } else {
             $('.fa-times').hide()
             $('.fa-bars').show()
         }
-    
     });
-    
 });
-//accordeon
+//accordion
+$('.ui.accordion').accordion();
+$('.accordion').accordion({
+        selector: {
+            trigger: '.title'
+        }
+    });
 
-  $('.ui.accordion').accordion();
-  $('.accordion')
-  .accordion({
-    selector: {
-      trigger: '.title'
+$('#title2 .icon').css('background-image', "url('image/icon-arrow-pink.png')");
+$('#title1, #title2, #title3, #title4, #title5').click(function () {
+    if ($(this).hasClass('active')) {
+        $('#' + $(this).attr('id') + ' .icon').css('background-image', "url('image/icon-arrow.png')");
+    } else {
+        $('#title1 .icon').css('background-image', $(this).attr('id') === 'title1' ? "url('image/icon-arrow-pink.png')" : "url('image/icon-arrow.png')");
+        $('#title2 .icon').css('background-image', $(this).attr('id') === 'title2' ? "url('image/icon-arrow-pink.png')" : "url('image/icon-arrow.png')");
+        $('#title3 .icon').css('background-image', $(this).attr('id') === 'title3' ? "url('image/icon-arrow-pink.png')" : "url('image/icon-arrow.png')");
+        $('#title4 .icon').css('background-image', $(this).attr('id') === 'title4' ? "url('image/icon-arrow-pink.png')" : "url('image/icon-arrow.png')");
+        $('#title5 .icon').css('background-image', $(this).attr('id') === 'title5' ? "url('image/icon-arrow-pink.png')" : "url('image/icon-arrow.png')");
     }
-  })
-;
-$('.title1').click(function(){
-    if(!$(this).hasClass('active')){
-        $('.title1 .icon').css('background-image', "url('/image/icon-arrow-pink.png')");
-        $('.title2 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title3 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title4 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title5 .icon').css('background-image', "url('/image/icon-arrow.png')")
-    }
-    else{
-        $('.title1 .icon').css('background-image', "url('/image/icon-arrow.png')");
-    }
-})
-$('.title2').click(function(){
-    $('.title2 .icon').css('background-image', "url('/image/icon-arrow-pink.png')");
-    if(!$(this).hasClass('active')){
-        $('.title2 .icon').css('background-image', "url('/image/icon-arrow-pink.png')");
-        $('.title1 .icon').css('background-image', "url('/image/icon-arrow.png')");
-        $('.title3 .icon').css('background-image', "url('/image/icon-arrow.png')");
-        $('.title4 .icon').css('background-image', "url('/image/icon-arrow.png')");
-        $('.title5 .icon').css('background-image', "url('/image/icon-arrow.png')");
-    }
-    else{
-        $('.title2 .icon').css('background-image', "url('/image/icon-arrow.png')");
-    }
-})
-$('.title3').click(function(){
-    if(!$(this).hasClass('active')){
-        $('.title3 .icon').css('background-image', "url('/image/icon-arrow-pink.png')");
-        $('.title1 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title2 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title4 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title5 .icon').css('background-image', "url('/image/icon-arrow.png')")
-    }
-    else{
-        $('.title3 .icon').css('background-image', "url('/image/icon-arrow.png')");
-    }
-})
-$('.title4').click(function(){
-    if(!$(this).hasClass('active')){
-        $('.title4 .icon').css('background-image', "url('/image/icon-arrow-pink.png')");
-        $('.title1 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title2 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title3 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title5 .icon').css('background-image', "url('/image/icon-arrow.png')")
-    }
-    else{
-        $('.title4 .icon').css('background-image', "url('/image/icon-arrow.png')");
-    }
-})
-$('.title5').click(function(){
-    if(!$(this).hasClass('active')){
-        $('.title5 .icon').css('background-image', "url('/image/icon-arrow-pink.png')");
-        $('.title1 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title2 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title3 .icon').css('background-image', "url('/image/icon-arrow.png')")
-        $('.title4 .icon').css('background-image', "url('/image/icon-arrow.png')")
-    }
-    else{
-        $('.title5 .icon').css('background-image', "url('/image/icon-arrow.png')");
-    }
-})
+});
 
 
+
+
+//slider
+$(window).on('load resize', function() { 
+    if ($(window).width() < 768) {
+      $('.news-feed').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 100,
+        slidesToShow: 1
+      });
+    }
+    else{
+        $(".news-feed").slick("unslick");
+    }
+  });
+// accordion footer
+// $(window).on('load resize', function() {
+//     if ($(window).width() < 640){
+//         $('.footer-menu').accordion({
+//             selector: {
+//                 accordion : '.footer-menu',
+//                 title     : '.footer-menu__title',
+//                 trigger   : '.footer-menu__title',
+//                 content   : '.footer-menu-list'
+//             }
+//         })
+//     }
+// })
+
+$('.ui.accordion').accordion();
+$('.accordion').accordion({
+        selector: {
+            accordion : '.footer-menu',
+            title     : '.footer-menu__title',
+            trigger   : '.footer-menu__title',
+            content   : '.footer-menu-list'
+        }
+    });
